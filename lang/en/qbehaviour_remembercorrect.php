92ed5fb68f6accde8e99b49828eb28ea2ff7fe89
Benjamin Walker <benjaminwalker@catalyst-au.net>
2026-01-16 15:37:49 +1000

    Add behaviour plugin skeleton extending deferredfeedback

---
+++ lang/en/qbehaviour_remembercorrect.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qbehaviour_remembercorrect', language 'en'.
+ *
+ * @package    qbehaviour_remembercorrect
+ * @author     Benjamin Walker <benjaminwalker@catalyst-au.net>
+ * @copyright  2026, Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Remember correct across attempts';
+$string['privacy:metadata'] = 'The remember correct question behaviour plugin does not store any personal data.';
+$string['questionusedunknownmodel'] = 'This question was attempted with a behaviour that is not currently available. The question is being displayed as well as possible, but some parts may be remembercorrect or wrong.';
